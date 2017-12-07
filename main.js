fa4d7d68d905e6ed9808911854ee93625549b3e5
Atomnus <33552217+Atomnus@users.noreply.github.com>
2017-12-07 14:48:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function unlock() {
   pass_enter = document.getElementById('password').value;
   if(pass_enter == '920463') {
     window.location.href = 'ylial.html';
+  }else {
+    alert('Passcode is incorrect!');
   }
 }
